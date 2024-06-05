e925e4191033230d834d23a52fe22cee9b839a37
Dhanada <dhanada.santika@gmail.com>
2024-06-05 18:28:29 +0700

    address

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,7 +1,11 @@
-import { SignIn } from "@clerk/nextjs";
-import Image from "next/image";
+"use client";
 
+import Image from "next/image";
 import React from "react";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@radix-ui/react-label";
 
 export default function ProfilePage() {
   return (
@@ -14,6 +18,26 @@ export default function ProfilePage() {
           alt="login"
           className="h-7/12 w-7/12"
         />
+      </div>
+      <div className="flex flex-col w-full scale-[90%] items-center justify-center lg:w-1/2">
+        <h1 style={{ fontSize: 40, fontWeight: "bold" }} className="mb-[60px]">Pengiriman</h1>
+        <div className="flex flex-col justify-between gap-3">
+          <div>
+          <Label style={{fontSize: 12, fontWeight: "bold"}}>Nama Lengkap</Label>
+          <Input className="w-[576px] h-9" type="Nama" placeholder="Nama Lengkap" style={{ fontSize: '12px'}}/>
+          </div>
+          <div>
+          <Label style={{fontSize: 12, fontWeight: "bold"}}>Nomor Telepon</Label>
+          <Input className="w-[576px] h-9" type="Nama" placeholder="Nomor Telepon" style={{ fontSize: '12px'}}/>
+          </div>
+          <div>
+          <Label style={{fontSize: 12, fontWeight: "bold"}}>Alamat</Label>
+          <Input className="w-[576px] h-[106px]" type="Nama" placeholder="Please enter your address" style={{ fontSize: '12px'}}/>
+          </div>    
+        </div>   
+        <div className="mt-4">
+        <Button>Save</Button>
+        </div>
       </div>
     </div>
   );
